0a8949b42a08da02eb0b8496f95a672e9247991c
MartinSimango <shukosimango@yahoo.com>
2018-07-23 20:52:56 +0200

    create suggested board

--- Sudoku1/Sudoku1/Program.cs
+++ Sudoku1/Sudoku1/Program.cs
@@ -22,11 +22,15 @@ namespace Sudoku1
                 System.Environment.Exit(1);
             }
 
-            
+            IntSet numberSet = new IntSet(1, 2, 3, 4, 5, 6, 7, 8, 9);
             string [,] assingedBoard = new string[9,9]; //read in the assigned board
-            string [,] solutionBoard = new string [9,9];
+            string [,] solutionBoard = new string[9,9];
 
+            int [,][,] suggestBoard = new int[9,9][,];
+           // List<IntSet[,]> suggestBoard = new List<IntSet[,]>();
+           // IntSet [,] generatedBoard = new IntSet[9,9]; 
 
+           
             //read the already assinged board
             readBoard(assingedBoard, data, false);
             Console.WriteLine("Already Assigned\n");
@@ -34,6 +38,11 @@ namespace Sudoku1
             readBoard(solutionBoard, data, true);
             Console.WriteLine("Solution Board\n");
             printAssignedBoard(solutionBoard);
+
+            initializeSuggestedBoard(suggestBoard);
+            updateSuggestedBoard(assingedBoard,suggestBoard);
+            
+            printSuggestionBoard(suggestBoard);
 
 
             Console.ReadKey();
@@ -73,12 +82,31 @@ namespace Sudoku1
                 Console.WriteLine(data.ReadLine());
             }
         }
-        static void printSuggestionBoard(string[,] board)
+        static string getSingleBlock(int [,] block,int index) //converts a co-ordinate (row and col) into a string of suggested numbers
+        {
+            string retVal = "";
+            for(int i = 0; i < 3; i++)
+            {
+                if (block[index, i] == 0)
+                {
+                    retVal += " ";
+                }
+                else
+                {
+                    retVal += block[index, i] + "";
+                }
+                
+            }
+            return retVal;
+        }
+        static void printSuggestionBoard(int[,][,] board)
         {
             //print the header
             string num; //num to print row
             Console.WriteLine("        0   1   2     3   4   5     6   7   8 \n"+
                               "     |=============+=============+=============|");
+            int row = 0;
+            int col = 0;
             for (int i = 1; i <= 27; i++)
             {
                 num = "  ";
@@ -87,11 +115,16 @@ namespace Sudoku1
                     num = " "+ i/3;
                    
                 }
-                string s = String.Format(" {0}  | {1}.{2}.{3} | {4}.{5}.{6} | {7}.{8}.{9} |",
-                                         num, "123", "123", "123", "123", "123", "123", "123", "123", "123");
+                
+                string s = String.Format(" {0}  | {1}.{2}.{3} | {4}.{5}.{6} | {7}.{8}.{9} |", num,
+                                         getSingleBlock(board[row,0],(i-1)%3), getSingleBlock(board[row, 1], (i - 1) % 3), getSingleBlock(board[row, 2], (i - 1) % 3),
+                                         getSingleBlock(board[row, 3], (i - 1) % 3), getSingleBlock(board[row, 4], (i - 1) % 3), getSingleBlock(board[row, 5], (i - 1) % 3),
+                                         getSingleBlock(board[row, 6], (i - 1) % 3), getSingleBlock(board[row, 7], (i - 1) % 3) , getSingleBlock(board[row, 8], (i - 1) % 3));
                 Console.WriteLine(s);
+
                 if (i % 3 == 0 && i!=27 )
-                { 
+                {
+                    row++;
                     Console.WriteLine("     |----+---+----+----+---+----+----+---+----| ");
                 }
             }
@@ -100,8 +133,41 @@ namespace Sudoku1
 
             
 
-      
                 
+        }
+        static void initializeSuggestedBoard(int[,][,] board)
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    board[row, col] = new int[3,3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+                   
+                }
+            }     
+        }
+
+        static void updateSuggestedBoard(string[,] assignedBoard, int[,][,] board)
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    if (assignedBoard[row, col] != "..")
+                    {
+                        for (int i = 0; i < 3; i++)
+                        {
+                            for (int j = 0; j < 3; j++)
+                            {
+                                board[row, col][i, j] = 0;
+                            }
+                        }
+             
+
+                    }
+                    
+                }
+            }
         }
         static void printAssignedBoard(string [,] board)
         {
